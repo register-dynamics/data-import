86faab99075f819011f66c0577c2d5d30668b845
Ross Jones <ross@register-dynamics.co.uk>
2024-08-13 11:36:28 +0100

    Re-layout to clean up

--- prototypes/basic/app/routes.js
+++ prototypes/basic/app/routes.js
@@ -2,9 +2,10 @@
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
-const importer = require("importer");
 const govukPrototypeKit = require("govuk-prototype-kit");
 const router = govukPrototypeKit.requests.setupRouter();
+
 const cfg = require("govuk-prototype-kit/lib/config");
+const importer = require("importer");
 
 importer.Initialise(cfg.getConfig(), router);
