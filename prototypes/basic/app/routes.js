b85552ac38d2b84ea47a185fe8491d522ccdedcd
Ross Jones <ross@register-dynamics.co.uk>
2024-08-29 11:14:56 +0100

    Macro for table assets (#21)
    When using a scoped package (@register-dynamics/importer) the path to
    load the assets is no longer plugin-assets/importer but now includes the
    scoped package name.
    
    To resolve this we have introduced a new macro for loading the table
    assets directly, without the user needing to know what they are.  This
    might be prettier if we had an obvious way to get the name of the
    package the code lives in.  We might in future load the package.json and
    read/encode and use it.

--- prototypes/basic/app/routes.js
+++ prototypes/basic/app/routes.js
@@ -6,6 +6,6 @@ const govukPrototypeKit = require("govuk-prototype-kit");
 const router = govukPrototypeKit.requests.setupRouter();
 
 const cfg = require("govuk-prototype-kit/lib/config");
-const importer = require("importer");
+const importer = require("@register-dynamics/importer");
 
 importer.Initialise(cfg.getConfig(), router, govukPrototypeKit);
