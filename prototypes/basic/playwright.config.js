3f5640712cec6a25254c2a0914384b531dd185f3
Ross Jones <ross@register-dynamics.co.uk>
2024-09-19 17:04:02 +0100

    Add CI for UI tests

--- prototypes/basic/playwright.config.js
+++ prototypes/basic/playwright.config.js
@@ -3,21 +3,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     use: {
-        // Base URL to use in actions like `await page.goto('/')`.
         baseURL: 'http://127.0.0.1:3000',
     },
     webServer: {
         command: 'npm run dev',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
-        stdout: 'ignore',
+        stdout: 'pipe',
         stderr: 'pipe',
-      },
-    projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-            dependencies: [],
-        },
-    ]
+      }
 });
