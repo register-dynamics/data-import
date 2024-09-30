58bd9c12a9f111180a211a0d77ef6839a267df48
Ross Jones <ross@register-dynamics.co.uk>
2024-09-30 11:34:03 +0100

    Constrain number of workers

--- prototypes/basic/playwright.config.js
+++ prototypes/basic/playwright.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+    workers: 1,
     testDir: './tests',
     reporter: process.env.CI ? [['github'], ['junit', {outputFile: "./test-results/results.xml"}]] : 'line',
     use: {
