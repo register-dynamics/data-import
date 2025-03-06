47d41209c55b5b74a61cf9c816d7fe2352ad0fd5
Ross Jones <ross@register-dynamics.co.uk>
2025-03-06 17:01:09 +0000

    Fix tests

--- prototypes/basic/tests/simple.spec.js
+++ prototypes/basic/tests/simple.spec.js
@@ -4,5 +4,5 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Import spreadsheet data/);
+  await expect(page).toHaveTitle(/Upload monthly pension return/);
 });
