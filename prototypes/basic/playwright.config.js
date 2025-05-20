d7eca84a8eaadd13fb8e0289c766a8c6327b8883
Ross Jones <ross@register-dynamics.co.uk>
2025-05-20 14:46:22 +0100

    Move tests and add multiple-projects to test config

--- prototypes/basic/playwright.config.js
+++ prototypes/basic/playwright.config.js
@@ -3,15 +3,38 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     workers: 1,
     testDir: './tests',
-    reporter: process.env.CI ? [['github'], ['junit', {outputFile: "./test-results/results.xml"}]] : 'line',
-    use: {
-        baseURL: 'http://127.0.0.1:3000',
-    },
+    reporter: process.env.CI ? [['github'], ['junit', { outputFile: "./test-results/results.xml" }]] : 'line',
+    // use: {
+    //     baseURL: 'http://127.0.0.1:3000',
+    // },
+    projects: [
+        {
+            name: 'simple-tests',
+            use: {
+                baseURL: 'http://localhost:3000',
+            },
+            testDir: './tests/simple',
+        },
+        {
+            name: 'sheet-selection-auto',
+            use: {
+                baseURL: 'http://localhost:3000',
+            },
+            testDir: './tests/sheet-selection-auto',
+        },
+        {
+            name: 'sheet-selection-manual',
+            use: {
+                baseURL: 'http://localhost:3000',
+            },
+            testDir: './tests/sheet-selection-manual',
+        },
+    ],
     webServer: {
         command: 'npm run dev',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
         stdout: 'pipe',
         stderr: 'pipe',
-      }
+    }
 });
